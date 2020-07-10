310899890085e7103b676a2d0400a631ca911bf6
tonysathre <tony@ghostbit.org>
2020-07-10 03:01:01 -0500

    Updated $Years

--- Get-PowerBallResults.ps1
+++ Get-PowerBallResults.ps1
@@ -1,5 +1,5 @@
 
-$Years = 2019..$((Get-Date).Year)
+$Years = 1992..$((Get-Date).Year)
 $AllResults = @()
 $DrawDates = @()
 $regex = '^(?:(((Jan(uary)?|Ma(r(ch)?|y)|Jul(y)?|Aug(ust)?|Oct(ober)?|Dec(ember)?)\ 31)|((Jan(uary)?|Ma(r(ch)?|y)|Apr(il)?|Ju((ly?)|(ne?))|Aug(ust)?|Oct(ober)?|(Sept|Nov|Dec)(ember)?)\ (0?[1-9]|([12]\d)|30))|(Feb(ruary)?\ (0?[1-9]|1\d|2[0-8]|(29(?=,\ ((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)))))))\,\ ((1[6-9]|[2-9]\d)\d{2}))'
